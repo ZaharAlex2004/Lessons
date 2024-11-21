f893a7c6f96ea9af0b9344fd84bd63aba123f7a7
BuildTools <unconfigured@null.spigotmc.org>
2024-11-21 19:21:32 +0200

    python_lessons

--- Lesson_4_3.py
+++ Lesson_4_3.py
@@ -1,10 +1,8 @@
 import random
 
-lst = [1, 2, 3, 4, 5, 6, 7, 9]
-#lst = [6, 3, 7]
-#lst = [1, 1, 2, 1]
-n = len(lst)
-for i in range(n):
-    lst.append(random.randint(3, 10))
-    lt1, lt2, lt3 = lst[0], lst[2], lst[-1]
+lst = [random.randint(3, 10) for i in range(random.randint(3, 10))]
+print(lst)
+
+for i in range(random.randint(3, 10)):
+    lt1, lt2, lt3 = lst[0], lst[2], lst[-2]
 print([lt1, lt2, lt3])
