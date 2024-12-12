9b289c3ddb6fbe6fd86980206a178b5da28b2a2c
BuildTools <unconfigured@null.spigotmc.org>
2024-12-12 13:19:39 +0200

    python_lessons

---
+++ Lesson_10.py
@@ -0,0 +1,19 @@
+def pow(x):
+    return x ** 2
+
+
+def some_gen(begin, end, func):
+    for n in range(end):
+        yield begin
+        begin = func(begin)
+
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+print(isgenerator(gen))
+print(list(some_gen(2, 4, pow)))
+
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
