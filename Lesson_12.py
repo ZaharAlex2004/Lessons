daf0f8fde6da3a6187ab49e0a7a1582bee586f55
BuildTools <unconfigured@null.spigotmc.org>
2024-12-19 19:14:53 +0200

    python_lessons

---
+++ Lesson_12.py
@@ -0,0 +1,13 @@
+import codecs
+import re
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+        r = re.sub(r'<[^>]+>', '', html)
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(r)
+
+
+print(delete_html_tags('draft.html'))
