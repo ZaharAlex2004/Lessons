02c3f1166b82485cbbb0f9a9256112d98da6ca04
BuildTools <unconfigured@null.spigotmc.org>
2025-01-15 09:55:07 +0200

    python_lessons

--- Pro/L1_Radius.py
+++ Pro/L1_Radius.py
@@ -1,34 +1,9 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2*(self.width + self.height)
-
-    def is_square(self):
-        if self.width != self.height:
-            return True
-        else:
-            return False
-
-    def resize(self, new_width, new_height):
-        self.width = new_width
-        self.height = new_height
-
-    def __repr__(self):
-        return f"ширина = {self.width}, высота = {self.height}"
+# Инициализируем функцию вычисления площади круга
+# radius - радиус круга
+def calculate_circle_area(radius):
+    return 3.14 * radius ** 2 # Здесь мы вычисляем площадь круга
 
 
-rd = Rectangle(30, 5)
-print(rd)
-print('Площадь: ', rd.area())
-print('Периметр: ', rd.perimeter())
-print(rd.is_square())
-rd.resize(56, 20)
-print(rd)
-print('Площадь: ', rd.area())
-print('Периметр: ', rd.perimeter())
+rad = float(input("Введите радиус: "))  # Вводим радиус круга
+res = calculate_circle_area(rad)  # Вычисляем
+print(res)  # Выводим площадь круга
--- Pro/L1_Rectangle.py
+++ Pro/L1_Rectangle.py
@@ -1,7 +1,42 @@
-def calculate_circle_area(radius):
-    return 3.14 * radius ** 2
+# Создаём класс Прямоугольника
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width  # Ширина
+        self.height = height  # Высота
+
+    # Вычисляем площадь
+    def area(self):
+        return self.width * self.height
+
+    # Вычисляем периметр
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    # Определяем, квадрат или прямоугольник
+    def is_square(self):
+        if self.width != self.height:
+            return True  # Прямоугольник
+        else:
+            return False  # Квадрат
+
+    # Изменяем размер прямоугольника
+    def resize(self, new_width, new_height):
+        self.width = new_width
+        self.height = new_height
+
+    # Для вывода ширины и высоты
+    def __str__(self):
+        return f"ширина = {self.width}, высота = {self.height}"
 
 
-rad = float(input("Введите радиус: "))
-res = calculate_circle_area(rad)
-print(res)
+rd = Rectangle(30, 5)  # Назначаем ширину и высоту
+print(rd)  # Выводим ширину и высоту
+print('Площадь: ', rd.area())  # Выводим площадь
+print('Периметр: ', rd.perimeter())  # Выводим периметр
+print(rd.is_square())  # Выводим его точность
+rd.resize(56, 20)  # Меняем размер
+# И снова выводим площадь
+print(rd) # Выводим ширину и высоту
+print('Площадь: ', rd.area()) # Выводим площадь
+print('Периметр: ', rd.perimeter()) # Выводим периметр
+print(rd.is_square())  # Выводим его точность
