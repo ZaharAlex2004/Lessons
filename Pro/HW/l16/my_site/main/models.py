47d22008f36e3bcaaad5148d1fa5f45cca252332
BuildTools <unconfigured@null.spigotmc.org>
2025-03-15 17:33:55 +0200

    python_lessons

--- Pro/HW/l16/my_site/main/models.py
+++ Pro/HW/l16/my_site/main/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 
-#
+class Service(models.Model):
+    name = models.TextField()
+    price = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}, {self.price}"
